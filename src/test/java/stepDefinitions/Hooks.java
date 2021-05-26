c2e520ea0024ef879e2f6dc84413d55ccddcfaad
arnis <arnisk999@gmail.com>
2021-05-26 11:00:38 +0300

    Initial commit

---
+++ src/test/java/stepDefinitions/Hooks.java
@@ -0,0 +1,41 @@
+package stepDefinitions;
+
+import io.cucumber.core.api.Scenario;
+import io.cucumber.java.After;
+import io.cucumber.java.Before;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebDriverException;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.net.MalformedURLException;
+import java.util.concurrent.TimeUnit;
+
+public class Hooks {
+    public static WebDriver driver;
+    static String libWithDriversLocation = System.getProperty("user.dir") + "\\lib\\";
+
+    @Before
+    public void openBrowser() throws MalformedURLException {
+        System.setProperty("webdriver.chrome.driver", libWithDriversLocation + "chromedriver.exe");
+        driver = new ChromeDriver();
+        driver.manage().deleteAllCookies();
+        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+        driver.manage().window().maximize();
+    }
+
+    @After
+    public void embedScreenshot(Scenario scenario) {
+        if (scenario.isFailed()) {
+            try {
+                scenario.write("Current Page URL is " + driver.getCurrentUrl());
+                byte[] screenshot = ((TakesScreenshot) driver).getScreenshotAs(OutputType.BYTES);
+                scenario.embed(screenshot, "image/png");
+            } catch (WebDriverException somePlatformsDontSupportScreenshots) {
+                System.err.println(somePlatformsDontSupportScreenshots.getMessage());
+            }
+        }
+        driver.quit();
+    }
+}
